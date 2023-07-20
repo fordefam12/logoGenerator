6c9459c0b83518fe933bdeeb2176e2f35b6fe697
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-07-20 14:09:58 -0500

    fileName formating complete

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ function writeToFile(FileName, answers) {
   svg ='<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
   svg += "<g>";
   svg += `${answers.Shape}`;
-
+// console.log(answers.Shape);
   let shapeChoice;
-  if (answers.shape === "Triangle") {
+  if (answers.Shape === "Triangle") {
     shapeChoice = new Triangle();
     svg += `<polygon points="150,18 244, 182 56,182" fill="${answers.shapeBackgroundColor}"/>`;
-  } else if (answers.shape === "Square") {
+  } else if (answers.Shape === "Square") {
     shapeChoice = new Square();
     svg += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
   } else {
@@ -23,44 +23,55 @@ function writeToFile(FileName, answers) {
   svg += "<g>";
   svg += "</svg>";
 
-  fs.writeFile(FileName, svg, (err) => {
-    err ? console.log(err) : console.log("generated logo.svg");
+  fs.writeFile(FileName,svg, (err) => {
+    err ? console.log(err) : console.log("generated shape");
+    
+   
+   
   });
 }
-function promptUser() {
+ function promptUser() {
   inquirer
     .prompt([
+        {
+            type: "list",
+            message: "what shape would you like the logo to be?",
+            name: "Shape",
+            choices:["Triangle","Square","Circle"],
+          },
         {
             type: "Input",
             message: "choose shape color...",
             name: "shapeBackgroundColor",
           },
-      {
-        type: "Input",
-        message: "what text would you like your logo to display ?",
-        name: "text",
-      },
+     
       {
         type: "Input",
         message: "choose Text color...",
         name: "textColor",
       },
+      
       {
-        type: "input",
-        message: "what shape would you like the logo to be?",
-        name: "shape",
-        choices:["Triangle","Square","Circle"],
+        type: "Input",
+        message: "what text would you like your logo to display ?",
+        name: "text",
       },
       
      
     ])
+    
     .then((answers) => {
       if (answers.text.length > 3) {
         console.log("must enter a value of no more than 3 characters");
         promptUser();
       } else {
-        writeToFile("./examples/logo.svg", answers);
+        // fs.createWriteStream(`./examples/${answers.Shape}.svg`, answers),
+        writeToFile(`./examples/${answers.Shape}.svg`, answers);
+        // fs.createWriteStream(`./examples/${answers.Shape}.svg`);
       }
+    
     });
 }
+
 promptUser();
+
